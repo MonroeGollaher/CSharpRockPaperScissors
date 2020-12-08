c83c701b0e0eafee8466c3dc900c3ef441aea50c
Monroe Gollaher <mgollaher92@gmail.com>
2020-12-07 21:00:31 -0700

    rock, paper, scissors with C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpRPS
 {
@@ -6,7 +7,78 @@ namespace CSharpRPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<string> rpsChoices = new List<string>();
+            
+            // GAME OPTIONS
+            rpsChoices.Add("Rock");
+            rpsChoices.Add("Paper");
+            rpsChoices.Add("Scissors");
+
+            // GAME SET TO NOT RUNNING
+            bool running = false;
+
+            // RANDOM CPU CHOICE
+            Random randomizer = new Random();
+            int randInt = randomizer.Next(0, 3);
+            string cpuChoice = rpsChoices[randInt].ToLower(); 
+
+            Console.WriteLine(@"Welcome to Rock, Paper, Scissors!");
+            Console.WriteLine("Please type in what option you would like to play.");
+
+            string userChoice = Console.ReadLine().ToLower();
+
+            while (!running)
+            {
+                System.Console.WriteLine($"Hello!");
+                running = true;
+            }
+
+            while(running)
+            {
+                switch(userChoice)
+                {
+                    case "rock":
+                        Console.WriteLine($"You picked {userChoice}, and the computer picked {cpuChoice}.");
+                        running = false;
+                        break;
+                    case "paper":
+                        Console.WriteLine($"You picked {userChoice}, and the computer picked {cpuChoice}.");
+                        running = false;
+                        break;
+                    case "scissors": 
+                        Console.WriteLine($"You picked {userChoice}, and the computer picked {cpuChoice}.");
+                        running = false;
+                        break;
+                    default: 
+                        Console.WriteLine("Please enter a valid choice.");
+                        break;
+                }    
+            }
+
+            if(userChoice == cpuChoice)
+            {
+                Console.WriteLine("It's a tie.");
+            } else if(userChoice == "rock" && cpuChoice == "paper")
+            {
+                Console.WriteLine("The computer won.");
+            } else if(userChoice == "rock" && cpuChoice == "scissors")
+            {
+                Console.WriteLine("You won!");
+            } else if(userChoice == "paper" && cpuChoice == "scissors")
+            {
+                Console.WriteLine("The computer won.");
+            } else if(userChoice == "paper" && cpuChoice == "rock")
+            {
+                Console.WriteLine("You won!");
+            } else if(userChoice == "scissors" && cpuChoice == "paper")
+            {
+                Console.WriteLine("You won!");
+            } else if(userChoice == "scissors" && cpuChoice == "rock")
+            {
+                Console.WriteLine("The computer won.");
+            }
         }
     }
 }
+
